5110fb59cb3b92e4d37f02609413df5ed26b5fc2
Akshit Gupta <akshitgupta29@gmail.com>
2020-05-02 23:36:04 +0530

    commit of 02.05

--- Week 3/BasicRegex.py
+++ Week 3/BasicRegex.py
@@ -1,3 +1,10 @@
 import re
 
-re.sea
+print1 = re.search (r"aza", 'bazaar')
+print (print1)
+
+print2 = re.search(r"a^", 'Akshit')
+print (print2)
+
+print3 = re.search(r"a", 'Akshit Gupta', re.IGNORECASE)
+print (print3)
