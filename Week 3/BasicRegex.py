38f4a8031bfdd93b744d84c79f4bb9f45009aa5d
Akshit Gupta <akshitgupta29@gmail.com>
2020-05-03 16:22:22 +0530

    added wildcards

--- Week 3/BasicRegex.py
+++ Week 3/BasicRegex.py
@@ -6,7 +6,9 @@ print (print1)
 print2 = re.search(r"a^", 'Akshit')
 print (print2)
 
-print3 = re.search(r"a", 'Akshit Gupta', re.IGNORECASE)
+print3 = re.search(r"t", 'Akshit Gupta', re.IGNORECASE)
 print (print3)
 
-print4 = re.search()
+print(re.search(r'p.ng', 'penguin'))
+print (re.search(r'p.ng', 'clapping' ))
+print (re.search(r'p.ng', 'sponge' ))
