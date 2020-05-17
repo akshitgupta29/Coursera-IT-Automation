34a334c0d6b79d0016e549f9b660afcfad773c41
Akshit Gupta <akshitgupta29@outlook.com>
2020-05-17 15:59:36 +0530

    test case

--- Week 5/exceptions_test.py
+++ Week 5/exceptions_test.py
@@ -7,4 +7,19 @@ class TestTryExpect (unittest.TestCase):
         minlen = 5
         self.assertEqual (valid_user(testcase, minlen), True)
 
+    def test_too_short (self):
+        testcase = "Ak"
+        minlen = 5
+        self.assertEqual(valid_user(testcase, minlen), False)
+    
+    def test_invalid_character (self):
+        testcase = "Akshit_Gupta"
+        minlen = 5
+        self.assertEqual(valid_user(testcase, minlen), False)
+
+    def test_invalid_len (self):
+        testcase = "Akshit"
+        minlen = -1
+        self.assertRaise
+
 unittest.main()
